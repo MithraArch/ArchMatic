a9bc2157beb18a38cc2685f18f6e72987f8dcb10
Mithra <50359897+MithraArch@users.noreply.github.com>
2020-08-28 09:10:54 +0530

    Update and rename archmatic.sh to MithraMatic.sh

--- MithraMatic.sh
+++ MithraMatic.sh
@@ -95,7 +95,7 @@ function baseSetup {
 
         # --- Setup Desktop
             'awesome'               # Awesome Desktop
-            'xfce4-power-manager'   # Power Manager 
+            #'xfce4-power-manager'   # Power Manager 
             'rofi'                  # Menu System
             'picom'                 # Translucent Windows
             'xclip'                 # System Clipboard
@@ -109,10 +109,7 @@ function baseSetup {
         # --- Networking Setup
             'wpa_supplicant'            # Key negotiation for WPA wireless networks
             'dialog'                    # Enables shell scripts to trigger dialog boxes
-            'openvpn'                   # Open VPN support
-            'networkmanager-openvpn'    # Open VPN plugin for NM
             'network-manager-applet'    # System tray icon/utility for network connectivity
-            'libsecret'                 # Library for storing passwords
         
         # --- Audio
             'alsa-utils'        # Advanced Linux Sound Architecture (ALSA) Components https://alsa.opensrc.org/
@@ -120,22 +117,15 @@ function baseSetup {
             'pulseaudio'        # Pulse Audio sound components
             'pulseaudio-alsa'   # ALSA configuration for pulse audio
             'pavucontrol'       # Pulse Audio volume control
-            'pnmixer'           # System tray volume control
 
-        # --- Bluetooth
-            'bluez'                 # Daemons for the bluetooth protocol stack
-            'bluez-utils'           # Bluetooth development and debugging utilities
-            'bluez-firmware'        # Firmwares for Broadcom BCM203x and STLC2300 Bluetooth chips
-            'blueberry'             # Bluetooth configuration tool
-            'pulseaudio-bluetooth'  # Bluetooth support for PulseAudio
-        
+          
         # --- Printers
-            'cups'                  # Open source printer drivers
-            'cups-pdf'              # PDF support for cups
-            'ghostscript'           # PostScript interpreter
-            'gsfonts'               # Adobe Postscript replacement fonts
-            'hplip'                 # HP Drivers
-            'system-config-printer' # Printer setup  utility
+            #'cups'                  # Open source printer drivers
+            #'cups-pdf'              # PDF support for cups
+            #'ghostscript'           # PostScript interpreter
+            #'gsfonts'               # Adobe Postscript replacement fonts
+            #'hplip'                 # HP Drivers
+            #'system-config-printer' # Printer setup  utility
     )
 
     for PKG in "${PKGS[@]}"; do
@@ -153,25 +143,11 @@ function softwareSetup {
         # TERMINAL UTILITIES --------------------------------------------------
 
         'bash-completion'       # Tab completion for Bash
-        'bleachbit'             # File deletion utility
-        'cronie'                # cron jobs
         'curl'                  # Remote content retrieval
-        'file-roller'           # Archive utility
-        'gtop'                  # System monitoring via terminal
-        'gufw'                  # Firewall manager
-        'hardinfo'              # Hardware info app
         'htop'                  # Process viewer
         'neofetch'              # Shows system info when you launch terminal
         'ntp'                   # Network Time Protocol to set time via network.
         'numlockx'              # Turns on numlock in X11
-        'openssh'               # SSH connectivity tools
-        'p7zip'                 # 7z compression program
-        'rsync'                 # Remote file sync utility
-        'speedtest-cli'         # Internet speed via terminal
-        'terminus-font'         # Font package with some bigger fonts for login terminal
-        'tlp'                   # Advanced laptop power management
-        'unrar'                 # RAR compression program
-        'unzip'                 # Zip compression program
         'wget'                  # Remote content retrieval
         'terminator'            # Terminal emulator
         'vim'                   # Terminal Editor
@@ -182,10 +158,8 @@ function softwareSetup {
 
         # DISK UTILITIES ------------------------------------------------------
 
-        'android-tools'         # ADB for Android
         'android-file-transfer' # Android File Transfer
         'autofs'                # Auto-mounter
-        'btrfs-progs'           # BTRFS Support
         'dosfstools'            # DOS Support
         'exfat-utils'           # Mount exFat drives
         'gparted'               # Disk utility
@@ -193,54 +167,26 @@ function softwareSetup {
         'gvfs-smb'              # More File System Stuff
         'nautilus-share'        # File Sharing in Nautilus
         'ntfs-3g'               # Open source implementation of NTFS file system
-        'parted'                # Disk utility
         'samba'                 # Samba File Sharing
-        'smartmontools'         # Disk Monitoring
         'smbclient'             # SMB Connection 
         'xfsprogs'              # XFS Support
 
         # GENERAL UTILITIES ---------------------------------------------------
 
         'flameshot'             # Screenshots
-        'freerdp'               # RDP Connections
-        'libvncserver'          # VNC Connections
         'nautilus'              # Filesystem browser
-        'remmina'               # Remote Connection
-        'veracrypt'             # Disc encryption utility
-        'variety'               # Wallpaper changer
 
         # DEVELOPMENT ---------------------------------------------------------
 
         'gedit'                 # Text editor
-        'clang'                 # C Lang compiler
         'cmake'                 # Cross-platform open-source make system
-        'code'                  # Visual Studio Code
-        'electron'              # Cross-platform development using Javascript
-        'git'                   # Version control system
-        'gcc'                   # C/C++ compiler
-        'glibc'                 # C libraries
-        'meld'                  # File/directory comparison
-        'nodejs'                # Javascript runtime environment
-        'npm'                   # Node package manager
-        'python'                # Scripting language
-        'yarn'                  # Dependency management (Hyper needs this)
-
-        # MEDIA ---------------------------------------------------------------
-
-        'kdenlive'              # Movie Render
-        'obs-studio'            # Record your screen
-        'celluloid'             # Video player
         
         # GRAPHICS AND DESIGN -------------------------------------------------
-
-        'gcolor2'               # Colorpicker
+        
         'gimp'                  # GNU Image Manipulation Program
-        'ristretto'             # Multi image viewer
 
         # PRODUCTIVITY --------------------------------------------------------
-
-        'hunspell'              # Spellcheck libraries
-        'hunspell-en'           # English spellcheck library
+        
         'xpdf'                  # PDF viewer
 
     )
@@ -249,18 +195,13 @@ function softwareSetup {
 
     # UTILITIES -----------------------------------------------------------
 
-    'i3lock-fancy'              # Screen locker
-    'synology-drive'            # Synology Drive
-    'freeoffice'                # Office Alternative
-    
-    # MEDIA ---------------------------------------------------------------
-
-    'screenkey'                 # Screencast your keypresses
-    'lbry-app-bin'              # LBRY Linux Application
+    'i3lock-fancy'                                      # Screen locker
+    'synology-drive'                                    # Synology Drive
+    'freeoffice' # 'libreoffice-fresh'                  # Office Alternative
 
     # COMMUNICATIONS ------------------------------------------------------
 
-    'brave-nightly-bin'         # Brave
+    'brave-bin'         # Brave
     
 
     # THEMES --------------------------------------------------------------
